8c9da7542807e13ee82ee7a77f72310060035e6c
Pratham Chintrate <102407481+Pratham-101@users.noreply.github.com>
2024-10-19 10:30:59 +0530

    PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,14 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const navigate = useNavigate();
-  const getEmail = localStorage.getItem("emailData");
-  const getPassword = localStorage.getItem("passwordData");
-
+  
   useEffect(() => {
-    if (!getEmail && !getPassword) {
+    if (!localStorage.getItem("emailData") || !localStorage.getItem("passwordData")) {
       navigate("/login");
     }
-  }, [getEmail, getPassword, navigate]);
+  }, [navigate]);
 
   return children;
 }
