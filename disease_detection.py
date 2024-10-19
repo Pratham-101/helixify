8164d569a030874f5f8122ec7d44e8c46cf5c556
eshaan-vimal <evk2.development@gmail.com>
2024-10-19 09:10:34 +0530

    add disease detection and drug recommendation version 1

---
+++ disease_detection.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+import disease_ml
+
+
+def get_disease_prs(features):
+
+    prs_values = {}
+
+    features['risk_allele_frequency'] = ((features['allele1'] == features['risk_allele']).astype(float) + (features['allele2'] == features['risk_allele']).astype(float))
+
+    refined_features = features.dropna(subset=['odds_ratio']).copy()
+    refined_features['mrs'] = features['risk_allele_frequency'] * np.log(features['odds_ratio'])
+
+    prs_values = refined_features.groupby('disease_name')['mrs'].sum().to_dict()
+    return prs_values
+
+
+def  get_disease_prediction(features, prs_values):
+
+    final_features = pd.DataFrame()
+    final_features['key'] = features['rsid']+ "_" + features['disease_name']
+
+    final_features['risk_allele_frequency'] = features['risk_allele_frequency'] / 2
+
+    # for disease in prs_values:
+    #     if(prs_values[disease] >= threshold[disease]):
+    #         final_features.loc[len(final_features.index)] = [disease, 1]
+    #     else:
+    #         final_features.loc[len(final_features.index)] = [disease, 0]
+
+
+    final_features = final_features.transpose()
+    final_features.columns = final_features.iloc[0]
+    final_features = final_features.drop(final_features.index[0]).reset_index(drop=True)
+
+    # dataset = pd.read_csv(r'Database\Dataset.csv')
+    # dataset = dataset[final_features.columns]
+    
+    # new_dataset = pd.concat([dataset, final_features], ignore_index=True)
+    # new_dataset.to_csv(r'Database\Dataset.csv', index=False)
+
+    predictions = disease_ml.model_output(final_features.iloc[:, :-10])
+    return predictions
+
+
+def get_disease_score():
+    pass
---
+++ drug_recommendation.py
@@ -0,0 +1,19 @@
+
+
+def calculate_efficacy(features, disease_insights):
+    
+    drug_efficacy = {}
+
+    relevant_diseases = [disease for disease, has_disease in disease_insights.items() if has_disease == 1]
+    features = features[features['disease_name'].isin(relevant_diseases)].copy() 
+
+    features['genotype'] = features['allele1'] + features['allele2']
+
+    features['weight'] = features.apply(lambda row: 1 if row['genotype'] in row['positive_response'] else (-1 if row['genotype'] in row['negative_response'] else 0), axis=1)
+    efficacy_grouped = features.groupby(['disease_name', 'drug'])['weight'].sum().reset_index()
+
+    for disease, drug_group in efficacy_grouped.groupby('disease_name'):
+        drug_efficacy[disease] = dict(zip(drug_group['drug'], drug_group['weight']))
+        drug_efficacy[disease] = sorted(drug_efficacy[disease], key=drug_efficacy[disease].get, reverse=True)
+
+    return drug_efficacy
