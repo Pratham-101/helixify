3ee64c92d594fd6c8787c1149e78cf33d15a2e48
Pratham Chintrate <102407481+Pratham-101@users.noreply.github.com>
2024-10-19 10:24:33 +0530

    Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import HomeRemedies from "./HomeRemedies";
 import HealthCharts from "./HealthCharts";
@@ -12,68 +11,25 @@ export default function Home() {
     <Box
       sx={{
         mt: 10,
-        ml: 30,
+        ml: { xs: 18, sm: 18, md: 22, lg: 30 },
         textAlign: "center",
-        [theme.breakpoints.down("md")]: {
-          ml: 22,
-        },
-        [theme.breakpoints.down("sm")]: {
-          ml: 18,
-        },
       }}
     >
-      <Typography
-        variant="h4"
-        sx={{
-          [theme.breakpoints.down("md")]: {
-            fontSize: 26,
-          },
-          [theme.breakpoints.down("sm")]: {
-            fontSize: 22,
-          },
-        }}
-        gutterBottom
-      >
-        Welcome to the
-      </Typography>
-      <Typography
-        variant="h4"
-        sx={{
-          [theme.breakpoints.down("md")]: {
-            fontSize: 26,
-          },
-          [theme.breakpoints.down("sm")]: {
-            fontSize: 22,
-          },
-        }}
-        gutterBottom
-      >
-        User Dashboard Panel
-      </Typography>
-      <Typography
-        variant="h5"
-        sx={{
-          [theme.breakpoints.down("md")]: {
-            fontSize: 22,
-          },
-          [theme.breakpoints.down("sm")]: {
-            fontSize: 18,
-          },
-        }}
-        gutterBottom
-      >
-        Have a Good Day!
-      </Typography>
+      {["Welcome to the", "User Dashboard Panel", "Have a Good Day!"].map((text, idx) => (
+        <Typography
+          key={idx}
+          variant={idx === 2 ? "h5" : "h4"}
+          sx={{ fontSize: { xs: 18, sm: 22, md: 26 } }}
+          gutterBottom
+        >
+          {text}
+        </Typography>
+      ))}
 
-      {/* Adding spacing between "Have a Good Day!" and the next section */}
-      <Box sx={{ mt: 10 }} />  {/* Adjust the marginTop value (mt) for more or less spacing */}
+      <Box sx={{ mt: 10 }} /> {/* Spacing */}
 
-      {/* Adding new sections to Home */}
       <HomeRemedies />
-      {/* <Stats/> */}
-      {/* <NewsPanel /> */}
       <HealthCharts />
-      
     </Box>
   );
 }
